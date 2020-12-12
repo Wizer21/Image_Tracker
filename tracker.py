804e66f49f564eb34420090415c65cd90db783b1
Wizer <simon.tessier8@gmail.com>
2020-12-12 21:25:56 +0100

    Remake algorithm

--- tracker.py
+++ tracker.py
@@ -48,7 +48,7 @@ def start_tracker(pixel_map, size, newstep, color_to_found):
 
         if is_pixel_matching(pixel):
             if not is_already_found([current_width, current_heigh]):  # La forme est elle déjà analysée
-                pixel_list = color_matched(pixel_map)  # Si non, J'analyse ma nouvelle forme
+                pixel_list = pixel_scanner(pixel_map)  # Si non, J'analyse ma nouvelle forme
                 if pixel_list == "item_already_found":
                     current_width += step
                     nbr_iterations += 1
@@ -61,88 +61,76 @@ def start_tracker(pixel_map, size, newstep, color_to_found):
         nbr_iterations += 1
 
 
-def color_matched(pixel_map):
-    my_range = step
-    pos_width = current_width
-    pos_heigh = current_heigh
+def pixel_scanner(pixel_map):
+    orientation = 0
+    next_pixel = orient_my_pixel(current_width, current_heigh, orientation)
 
-    iterations = 0
     current_shape = []
-    still_in_shape = True
-    while still_in_shape:
-        val = check_nearby_pixels(pos_width, pos_heigh, pixel_map, my_range)
+    iterations = 0
+    keep_loop = True
+    while keep_loop:  # Si je commence dans le vide
+        new_matching_pixel = start_scanning(next_pixel[0], next_pixel[1], orientation, pixel_map)  # Je vais chercher le pixel qui matche de la prochaine ligne
 
-        if val == "not_found":
-            my_big_range = round(my_range * 2)
-            val = check_nearby_pixels(pos_width, pos_heigh, pixel_map, my_big_range)
-            if val == "not_found":
-                my_big_range = round(my_range * 3)
-                val = check_nearby_pixels(pos_width, pos_heigh, pixel_map, my_big_range)
-                if val == "not_found":
-                    return current_shape
-
-        if val == "item_already_found":
+        if new_matching_pixel == "item_already_found":  # Objet déjà scanné
             return "item_already_found"
-
-        print(str(val))
-        print(str(iterations))
-        pos_width = val[0]
-        pos_heigh = val[1]
-        current_shape.append(val)
+        elif new_matching_pixel == "full":  # First pixel matched
+            orientation -= 1
+            if orientation == -1:
+                orientation = 0
+            next_pixel = orient_my_pixel(next_pixel[0], next_pixel[1], orientation)
+        elif new_matching_pixel == "void":  # Si je n'ai rien trouvé
+            orientation += 1
+            if orientation == 4:
+                orientation = 0
+            next_pixel = orient_my_pixel(next_pixel[0], next_pixel[1], orientation)
+        else:
+            next_pixel = new_matching_pixel
+            current_shape.append(new_matching_pixel)
+            next_pixel = orient_my_pixel(new_matching_pixel[0], new_matching_pixel[1], orientation)
 
         iterations += 1
-        if iterations > 500:
+        if iterations > 10000:
             return current_shape
 
 
-def check_nearby_pixels(checked_width, checked_height, pixel_map, my_range):
-    global lastClockPosition
+def orient_my_pixel(checked_width, checked_height, orientation):
+    half_step = round(step/2)
+    if orientation == 0:
+        return [checked_width + 1, checked_height - half_step]
+    if orientation == 1:
+        return [checked_width + half_step, checked_height + 1]
+    if orientation == 2:
+        return [checked_width - 1, checked_height + half_step]
+    if orientation == 3:
+        return [checked_width - half_step, checked_height - 1]
 
-    low_range = round(my_range * 0.1)
-    half_range = round(my_range * 0.6)
 
-    nearby_pixels = [[checked_width, checked_height - my_range],
-                     [checked_width + (low_range * 2), checked_height - (my_range - low_range)],
-                     [checked_width + half_range, checked_height - half_range],
-                     [checked_width + (my_range - low_range), checked_height - (low_range * 2)],
-                     [checked_width + my_range, checked_height],
-                     [checked_width + (my_range - low_range), checked_height + (low_range * 2)],
-                     [checked_width + half_range, checked_height + half_range],
-                     [checked_width + (low_range * 2), checked_height + (my_range - low_range)],
-                     [checked_width, checked_height + my_range],
-                     [checked_width - (low_range * 2), checked_height + (my_range - low_range)],
-                     [checked_width - half_range, checked_height + half_range],
-                     [checked_width - (my_range - low_range), checked_height + (low_range * 2)],
-                     [checked_width - my_range, checked_height],
-                     [checked_width - (my_range - low_range), checked_height - (low_range * 2)],
-                     [checked_width - half_range, checked_height - half_range],
-                     [checked_width - (low_range * 2), checked_height - (my_range - low_range)],
-                     ]
+def start_scanning(checked_width, checked_height, orientation, pixel_map):
+    if is_pixel_matching(pixel_map[checked_width, checked_height]):
+        return "full"
 
-    where_out = False
-    clock_path = 0
-    current_clock = lastClockPosition - 2
-    if current_clock < 0:
-        keep = current_clock
-        current_clock = 16 + current_clock
+    for i in range(step):
+        next_pixel = next_pixel_pos(checked_width, checked_height, orientation)
+        if is_pixel_matching(pixel_map[next_pixel[0], next_pixel[1]]):
+            if is_already_found([next_pixel[0], next_pixel[1]]):
+                return "item_already_found"
+            return [next_pixel[0], next_pixel[1]]
+        else:
+            checked_width = next_pixel[0]
+            checked_height = next_pixel[1]
 
-    while clock_path < 20:
-        if where_out:
-            if is_pixel_matching(pixel_map[nearby_pixels[current_clock][0], nearby_pixels[current_clock][1]]):
-                lastClockPosition = current_clock
-                if is_already_found(nearby_pixels[current_clock]):  # Regarde que je n'arrive pas dans une forme connue, durant le parcourt
-                    return "item_already_found"
-                return nearby_pixels[current_clock]
+    return "void"
 
-        if not is_pixel_matching(pixel_map[nearby_pixels[current_clock][0], nearby_pixels[current_clock][1]]):
-            where_out = True
 
-        clock_path += 1
-        current_clock += 1
-        if current_clock == 16:
-            current_clock = 0
-
-    return "not_found"
+def next_pixel_pos(checked_width, checked_height, orientation):
+    if orientation == 0:
+        return [checked_width, checked_height + 1]
+    if orientation == 1:
+        return [checked_width - 1, checked_height]
+    if orientation == 2:
+        return [checked_width, checked_height - 1]
+    if orientation == 3:
+        return [checked_width + 1, checked_height]
 
 
 def is_pixel_matching(pixel):
