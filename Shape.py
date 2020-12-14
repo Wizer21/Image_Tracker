c3ccb750cf5b9d11c56c7866aee078b0403978ee
Wizer <simon.tessier8@gmail.com>
2020-12-14 20:35:49 +0100

    New algorithm working

--- Shape.py
+++ Shape.py
@@ -6,51 +6,39 @@ class Shape:
         self.bot_left = [0, 0]
         self.bot_right = [0, 0]
         self.center = [0, 0]
-        self.point_cloud = newlist
+        self.point_cloud = []
         self.width = 0
         self.height = 0
-        self.build_shape(newlist)
         self.isEmpty = False
+        self.min_width = 0
+        self.min_height = 0
+        self.max_width = 0
+        self.max_height = 0
+        self.build_shape(newlist)
 
     def build_shape(self, newlist):
         total_widht = 0
         total_height = 0
 
-        min_width = 0
-        min_height = 0
-        max_width = 0
-        max_height = 0
-
         size_list = len(newlist)
 
-        if size_list < 2:
-            self.isEmpty = True
-            return
-
         for i in range(size_list):
-            total_widht += newlist[i][0]
-            total_height += newlist[i][1]
+            total_widht += newlist[i][0][0]
+            total_height += newlist[i][0][1]
+            self.check_max_values(newlist[i][0][0], newlist[i][0][1])
+            self.point_cloud.append(newlist[i][0])
+            total_widht += newlist[i][1][0]
+            total_height += newlist[i][1][1]
+            self.check_max_values(newlist[i][1][0], newlist[i][1][1])
+            self.point_cloud.append(newlist[i][1])
 
-            if max_width < newlist[i][0]:
-                max_width = newlist[i][0]
-                if min_width == 0:
-                    min_width = max_width
-            if max_height < newlist[i][1]:
-                max_height = newlist[i][1]
-                if min_height == 0:
-                    min_height = max_height
-            if min_width > newlist[i][0]:
-                min_width = newlist[i][0]
-            if min_height > newlist[i][1]:
-                min_height = newlist[i][1]
-
-        self.top_left = [min_width, min_height]
-        self.top_right = [max_width, min_height]
-        self.bot_left = [min_width, max_height]
-        self.bot_right = [max_width, max_height]
+        self.top_left = [self.min_width, self.min_height]
+        self.top_right = [self.max_width, self.min_height]
+        self.bot_left = [self.min_width, self.max_height]
+        self.bot_right = [self.max_width, self.max_height]
         self.center = [round(total_widht/size_list), round(total_height/size_list)]
-        self.width = max_width - min_width
-        self.height = max_height - min_height
+        self.width = self.max_width - self.min_width
+        self.height = self.max_height - self.min_height
 
     def is_in_range(self, point_to_check):
         in_width = self.top_left[0] <= point_to_check[0] <= self.bot_right[0]
@@ -58,3 +46,17 @@ class Shape:
         if in_width and in_height:
             return True
         return False
+
+    def check_max_values(self, new_widht, new_height):
+        if self.max_width < new_widht:
+            self.max_width = new_widht
+            if self.min_width == 0:
+                self.min_width = self.max_width
+        if self.max_height < new_height:
+            self.max_height = new_height
+            if self.min_height == 0:
+                self.min_height = self.max_height
+        if self.min_width > new_widht:
+            self.min_width = new_widht
+        if self.min_height > new_height:
+            self.min_height = new_height
--- tracker.py
+++ tracker.py
@@ -6,12 +6,16 @@ bottom_color = (0, 0, 0)
 step = 0
 
 max_height = 0
+shapes_list = []
+temporary_shapes = []
 
 def start_tracker(newpixel_map, size, newstep, mincolor, maxcolor):
     global max_height
     global bottom_color
     global top_color
     global step
+    global shapes_list
+    global temporary_shapes
 
     max_width = size[0]
     max_height = size[1]
@@ -24,6 +28,8 @@ def start_tracker(newpixel_map, size, newstep, mincolor, maxcolor):
 
     list_of_rows = {}
     last_row = []
+    shapes_list.clear()
+    temporary_shapes.clear()
 
     justmatched = False
     continue_analysis = True
@@ -52,35 +58,50 @@ def start_tracker(newpixel_map, size, newstep, mincolor, maxcolor):
 
 
 def compile_items(list_of_rows):
-    shapes_list = []
+    global shapes_list
+    global temporary_shapes
 
-    temporary_shapes = []
     height_iterator = 0
     while height_iterator <= max_height:
 
-        if height_iterator in list_of_rows:
+        if height_iterator in list_of_rows:  # Si j'ai des valeurs sur la ligne actuelle
             current_row = list_of_rows[height_iterator]
-            if len(temporary_shapes) != 0:
-                temporary_shapes = micro_compile(current_row, temporary_shapes)
-
-            for i in range(len(current_row)):
-                temporary_shapes.append([[list_of_rows[height_iterator][i][0], height_iterator], [list_of_rows[height_iterator][i][1], height_iterator]])
-        else:
-            if len(temporary_shapes) != 0:
-                for i in range(len(temporary_shapes)):
-                    shapes_list.append(Shape(temporary_shapes[i]))
-                temporary_shapes.clear()
+            if len(temporary_shapes) != 0:  # Si j'ai des formes en stock
+                micro_compile(current_row, height_iterator)  # Je check si j'ai des matchs
+            else:  # Si je n'ai pas de formes en stock, je les crées
+                for i in range(len(current_row)):
+                    temporary_shapes.append([[[list_of_rows[height_iterator][i][0], height_iterator], [list_of_rows[height_iterator][i][1], height_iterator]]])
+        else:  # Si une ligne est complétement vide, je compile toutes les formes
+            for i in range(len(temporary_shapes)):
+                shapes_list.append(Shape(temporary_shapes[i]))
+            temporary_shapes.clear()
 
         height_iterator += step
 
     return shapes_list
 
-def micro_compile(current_row, temporary_shapes):
-    for i in range(len(temporary_shapes)):
-        for x in range(len(current_row)):
-            if temporary_shapes[i][0][0] <= current_row[x][0] <= temporary_shapes[i][1][0] or temporary_shapes[i][0][0] <= current_row[x][1] <= temporary_shapes[i][1][0]:
-                temporary_shapes[i].insert(0, current_row[i])
-    return temporary_shapes
+def micro_compile(current_row, height_iterator):
+    global shapes_list
+    global temporary_shapes
+    trash = []
+
+    shapesize_list = len(temporary_shapes)
+    rowsize_list = len(current_row)
+    for i in range(shapesize_list):  # Je regarde dans mes formes en cour de construction
+        shape_matched = False
+        for x in range(rowsize_list):  # Si certaines matches avec la nouvelle row
+            if temporary_shapes[i][0][0][0] <= current_row[x][0] <= temporary_shapes[i][0][1][0] or temporary_shapes[i][0][0][0] <= current_row[x][1] <= temporary_shapes[i][0][1][0]:
+                temporary_shapes[i].insert(0, [[current_row[x][0], height_iterator], [current_row[x][1], height_iterator]])  # Si j'ai un match j'ajoute à la position 0 afin qu'elle devienne la nouvelle clé d'entrée dans la forme
+                trash.append(current_row[x])
+                shape_matched = True
+        if not shape_matched:  # Si une forme a 0 matches sur cette ligne, je la considère comme finie et je la compile
+            shapes_list.append(Shape(temporary_shapes[i]))
+            trash.append(temporary_shapes[i])
+    for i in trash:
+        del i
+    for i in current_row:  # Si certaines zones de row n'ont pas match, je crée de nouvelles formes
+        temporary_shapes.append([[[i[0], height_iterator], [i[1], height_iterator]]])
+
 
 def is_pixel_matching(pixel):
     return bottom_color[0] <= pixel[0] <= top_color[0] and bottom_color[1] <= pixel[1] <= top_color[1] and bottom_color[2] <= pixel[2] <= top_color[2]
