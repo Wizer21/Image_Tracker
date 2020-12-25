39ba864f4ad912305f00a87296166db49fe2e4ef
Wizer <simon.tessier8@gmail.com>
2020-12-25 23:18:05 +0100

    Softened width adaptation

--- Cam_tracker.py
+++ Cam_tracker.py
@@ -11,48 +11,40 @@ max_rgb = (0, 0, 0)
 min_diameter = 0
 step = 4
 adaptive_color = (0, 0, 0)
-initial_color = (0, 0, 0)
+mid_rgb = (0, 0, 0)
 color_iterator = 0
 color_range = 0
 variations = []
 
-min_color_limit = (0, 0, 0)
-max_color_limit = (0, 0, 0)
-
-stack_diameter = 0
-stack_diameter_iterator = 0
+red_to_green = [0, 0]
+red_to_blue = [0, 0]
 
 max_width = 0
 max_height = 0
 
-def new_pos(x, y, mid_rgb, new_color_range, new_width, new_height):
+
+def new_pos(x, y, new_mid_rgb, newcolor_range, new_width, new_height):
     global last_x
     global last_y
     global color_iterator
     global color_range
     global adaptive_color
-    global initial_color
+    global mid_rgb
     global min_diameter
     global variations
-    global min_color_limit
-    global max_color_limit
-    global stack_diameter
-    global stack_diameter_iterator
     global max_width
     global max_height
 
     last_x = x
     last_y = y
-    color_range = new_color_range
-    initial_color = mid_rgb
+    color_range = newcolor_range
+    mid_rgb = new_mid_rgb
     min_diameter = step
 
-    adaptive_color = mid_rgb
-    color_iterator = 0
-    cal_colors(mid_rgb)
-
-    min_color_limit = min_rgb
-    max_color_limit = max_rgb
+    adaptive_color = new_mid_rgb
+    color_iterator = 1
+    cal_colors()
+    ini_color()
 
     variations = [[0, -step],
                  [+step, -step],
@@ -63,9 +55,6 @@ def new_pos(x, y, mid_rgb, new_color_range, new_width, new_height):
                  [-step, 0],
                  [-step, -step]]
 
-    stack_diameter = 0
-    stack_diameter_iterator = 0
-
     max_width = new_width
     max_height = new_height
 
@@ -73,13 +62,9 @@ def new_pos(x, y, mid_rgb, new_color_range, new_width, new_height):
 def cam_tracker(pixel_map):
     global last_x
     global last_y
-    global adaptive_color
-    global color_iterator
     global min_diameter
-    global stack_diameter
-    global stack_diameter_iterator
 
-    cal_colors(adaptive_color)  # Calc min and max color from adaptive_color
+    cal_colors()  # Calc min and max color from adaptive_color
     define_starter(pixel_map)
 
     # Define start positions
@@ -116,47 +101,38 @@ def cam_tracker(pixel_map):
         positions[i][0] -= variations[i][0]
         positions[i][1] -= variations[i][1]
 
-    last_x = left[0] + int(((right[0] - left[0])/2))
+    last_x = left[0] + int(((right[0] - left[0])/2))  # NEXT STARTER
     last_y = top[1] + int(((bot[1] - top[1])/2))
 
-    if color_iterator != 0:
-        adaptive_color = (int(adaptive_color[0] / color_iterator), int(adaptive_color[1] / color_iterator), int(adaptive_color[2] / color_iterator))
-        if adaptive_color[0] < min_color_limit[0] or adaptive_color[1] < min_color_limit[1] or adaptive_color[2] < min_color_limit[2]:
-            adaptive_color = min_color_limit
-        elif adaptive_color[0] > max_color_limit[0] or adaptive_color[1] > max_color_limit[1] or adaptive_color[2] > max_color_limit[2]:
-            adaptive_color = max_color_limit
-        color_iterator = 0
-
-    new_top_left = top_left
-    if left[0] < top_left[0]:
+    if left[0] > top_left[0]:  # CORNERS TO LARGEST WAY
         top_left[0] = left[0]
-    if top[1] < top_left[1]:
+    if top[1] > top_left[1]:
         top_left[1] = top[1]
 
-    new_bot_right = bot_right
     if right[0] > bot_right[0]:
         bot_right[0] = right[0]
     if bot[1] > bot_right[1]:
         bot_right[1] = bot[1]
 
-    if right[0] - left[0] > 10:
-        min_diameter = right[0] - left[0]
-    if min_diameter > bot[1] - top[1] > 10:
-        min_diameter = bot[1] - top[1]
-    if min_diameter > bot_right[0] - top_left[0] > 10:
-        min_diameter = bot_right[0] - top_left[0]
-    if min_diameter > bot_right[1] - top_left[1] > 10:
-        min_diameter = bot_right[1] - top_left[1]
+    new_diameter = 0
+    if bot_right[0] - top_left[0] > 10:  # FIND MIN DIAMETER
+        new_diameter = bot_right[0] - top_left[0]
+    if new_diameter > bot_right[1] - top_left[1] > 10:
+        new_diameter = bot_right[1] - top_left[1]
 
-    stack_diameter += min_diameter
-    stack_diameter_iterator += 1
-    min_diameter = round(stack_diameter / stack_diameter_iterator)
+    print("new" + str(new_diameter))
+    print(str(min_diameter))
+    print(str(new_diameter - min_diameter))
+    if new_diameter > 10:
+        min_diameter += (new_diameter - min_diameter) * 0.01
 
-    print("DIAMETER " + str(min_diameter))
-    new_width = new_bot_right[0] - new_top_left[0]
-    new_height = new_bot_right[1] - new_top_left[1]
+    print("DIAMETER DIAMETER " + str(round(min_diameter)))
+    new_width = bot_right[0] - top_left[0]
+    new_height = bot_right[1] - top_left[1]
 
-    return new_top_left, new_bot_right, new_width, new_height, adaptive_color
+    update_adaptive_color()
+
+    return top_left, bot_right, new_width, new_height, adaptive_color
 
 
 def is_pixel_matching(pixel):
@@ -165,41 +141,70 @@ def is_pixel_matching(pixel):
 
     if min_rgb[0] <= pixel[0] <= max_rgb[0] and min_rgb[1] <= pixel[1] <= max_rgb[1] and min_rgb[2] <= pixel[2] <= max_rgb[2]:
 
-        red = int(adaptive_color[0]) + pixel[0]
-        green = int(adaptive_color[1]) + pixel[1]
-        blue = int(adaptive_color[2]) + pixel[2]
-
-        adaptive_color = (red, green, blue)
+        adaptive_color = (int(adaptive_color[0]) + pixel[0], int(adaptive_color[1]) + pixel[1], int(adaptive_color[2]) + pixel[2])
         color_iterator += 1
         return True
 
     return False
 
 
-def cal_colors(mid_rgb):
+def update_adaptive_color():
+    global adaptive_color
+    global color_iterator
+
+    if color_iterator != 0:
+        adaptive_color = (round(int(adaptive_color[0] / color_iterator)), round(int(adaptive_color[1] / color_iterator)), round(int(adaptive_color[2] / color_iterator)))
+        color_iterator = 1
+
+        # adaptive_red_green = adaptive_color[1] - adaptive_color[0]  # CHECK IF STILL SAME COLOR
+        # if not red_to_green[0] < adaptive_red_green < red_to_green[1]:
+        #     adaptive_color = (adaptive_color[0], red_to_green[0], adaptive_color[2])
+        #
+        # adaptive_red_blue = adaptive_color[2] - adaptive_color[0]
+        # if not red_to_blue[0] < adaptive_red_blue < red_to_blue[1]:
+        #     adaptive_color = (adaptive_color[0], red_to_blue[0], adaptive_color[2])
+
+
+def cal_colors():
     global min_rgb
     global max_rgb
 
     # Set Min Color
-    rgb = [mid_rgb[0], mid_rgb[1], mid_rgb[2]]
+    rgb = [adaptive_color[0], adaptive_color[1], adaptive_color[2]]
     for i in range(len(rgb)):
-        rgb[i] -= color_range
+        rgb[i] -= int(color_range)
         if rgb[i] < 0:
             rgb[i] = 0
     min_rgb = (rgb[0], rgb[1], rgb[2])
 
     # Set Max Color
-    rgb = [mid_rgb[0], mid_rgb[1], mid_rgb[2]]
+    rgb = [adaptive_color[0], adaptive_color[1], adaptive_color[2]]
     for i in range(len(rgb)):
-        rgb[i] += color_range
+        rgb[i] += int(color_range)
         if 255 < rgb[i]:
             rgb[i] = 255
     max_rgb = (rgb[0], rgb[1], rgb[2])
 
 
+def ini_color():
+    global red_to_green
+    global red_to_blue
+
+    # half_range = round(color_range / 2)
+    #
+    # red_to_green = [0, int(mid_rgb[1]) - int(mid_rgb[0])]
+    # red_to_green[0] = red_to_green[1] - half_range
+    # red_to_green[1] = red_to_green[1] + half_range
+    #
+    # red_to_blue = [0, int(mid_rgb[2]) - int(mid_rgb[0])]
+    # red_to_blue[0] = red_to_blue[1] - half_range
+    # red_to_blue[1] = red_to_blue[1] + half_range
+
+
 def new_color_range(new_value):
     global color_range
     color_range = new_value
+    ini_color()
 
 
 def define_starter(pixel_map):
@@ -207,10 +212,9 @@ def define_starter(pixel_map):
     global last_y
     global min_diameter
 
-    my_range = min_diameter
+    my_range = round(min_diameter)
 
-    color = pixel_map[last_y][last_x]
-    if is_pixel_matching((color[0], color[1], color[2])):
+    if validate_pixel(pixel_map, last_y, last_x):
         return
 
     for y in range(3):
@@ -239,7 +243,7 @@ def define_starter(pixel_map):
                         last_y += position_list[i][1] - variations[i][1]
                         return
 
-        my_range += min_diameter
+        my_range += round(min_diameter)
     print("lost")
 
 
